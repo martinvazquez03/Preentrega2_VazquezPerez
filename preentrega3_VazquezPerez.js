316d9e3353fa630eec7e22e1bd708a44b76ca1b8
Martin Vazquez Perez <martinvp2003@outlook.com.ar>
2024-08-20 17:41:22 -0300

    modificando carrito para tercera preentrega

--- preentrega3_VazquezPerez.js
+++ preentrega3_VazquezPerez.js
@@ -1,4 +1,4 @@
-const carrito = []
+const carrito = JSON.parse(localStorage.getItem("carrito")) || []
 
 const productos = [
     {
@@ -34,7 +34,7 @@ productos.forEach((producto) => {
     let div = document.createElement("div")
     div.classList.add("producto")
     div.innerHTML = `
-    <span>${producto.nombre}</span> <p> $${producto.precio}</p>
+    <span>${producto.nombre}</span> <strong>|</strong> <p> $${producto.precio}</p>
     `
 
     let boton = document.createElement("button")
@@ -51,11 +51,42 @@ productos.forEach((producto) => {
 })
 
 const agregar_al_carrito = (producto) => {
+
     if (producto.stock == 0) {
-        alert("Sin disponibilidad de producto debido a falta de stock.")
+        Swal.fire({
+            title: "Error al agregar a tu carrito.",
+            text: "Lo sentimos, no contamos con stock de este producto actualmente.",
+            color: "#38598b",
+            icon:"error",
+            iconColor : "#add8e6",
+            footer : "<p>Intentelo de nuevo en otro momento.</p>",
+            showConfirmButton : false,
+            showCloseButton : true,
+            position : "center"
+        })
     }else {
-        carrito.push(producto)
+        Toastify({
+            text: "Agregado al carrito",
+            duration: 1500,
+            close: true,
+            gravity: "top",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+            background: "#38598b",
+            color: "#fff",
+            },
+            onClick: function(){}
+        }).showToast();
+
+        let agregadoAlCarrito = carrito.find((item) => item.id === producto.id)
+        if (agregadoAlCarrito){
+            agregadoAlCarrito.cantidad++
+        } else {
+            carrito.push({...producto, cantidad: 1})
+        }
     }
+    checkeo_carrito()
 }
 
 
@@ -68,13 +99,16 @@ function checkeo_carrito() {
         agregados_carrito.classList.remove("d-none")
         carrito_vacio.classList.add("d-none")
 
-
         agregados_carrito.innerHTML = ""
         carrito.forEach((producto) => {
             let div = document.createElement("div")
             div.classList.add("producto")
             div.innerHTML = `
-            <span>${producto.nombre}</span> <p> $${producto.precio}</p>
+            <span>${producto.nombre}</span> 
+            <strong>|</strong> 
+            <span> Cantidad: ${producto.cantidad}</span> 
+            <strong>|</strong>
+            <p> $${producto.precio}</p>
             `
         
             let boton = document.createElement("button")
@@ -90,16 +124,39 @@ function checkeo_carrito() {
         })
     }
     suma_total()
+
+    localStorage.setItem("carrito", JSON.stringify(carrito))
 }
+checkeo_carrito()
 
 function quitar_del_carrito(producto) {
     const indice = carrito.findIndex((item) => item.id === producto.id)
-    carrito.splice(indice,1)
+    if (producto.cantidad > 1) {
+        producto.cantidad--
+    } else {
+        carrito.splice(indice,1)
+    }
+    
+
+    Toastify({
+        text: "Eliminado del carrito",
+        duration: 2000,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+        background: "#38598b",
+        color: "#fff",
+        },
+        onClick: function(){}
+    }).showToast();
+
     checkeo_carrito()
 }
 
 
 function suma_total() {
-    const total = carrito.reduce((acc,prod) => acc + prod.precio, 0)
+    const total = carrito.reduce((acc,prod) => acc + (prod.precio * prod.cantidad), 0)
     total_carrito.innerHTML = `<h4>Total: $${total}</h4>` 
 }
